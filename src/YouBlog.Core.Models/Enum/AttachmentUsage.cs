5d73dd8318d0d7a896f5100a1643d9cd8472a2a0
ElderJames <shunjiey@hotmail.com>
2016-07-15 00:57:26 +0800

    增加model

---
+++ src/YouBlog.Core.Models/Enum/AttachmentUsage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace YouBlog.Core.Models
+{
+    public enum AttachmentUsage
+    {
+        Aritcle,Head,Logo,Icon
+    }
+
+    public static class UsageString
+    {
+        public static Dictionary<AttachmentUsage, string> UsageList
+        {
+            get
+            {
+                return new Dictionary<AttachmentUsage, string>() { 
+                {AttachmentUsage.Aritcle,"文章"},
+                {AttachmentUsage.Head,"头像"},
+                {AttachmentUsage.Icon,"图标"},
+                {AttachmentUsage.Logo,"LOGO"}
+                };
+            }
+        }
+    }
+}
---
+++ src/YouBlog.Core.Models/Menu.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace YouBlog.Core.Models
+{
+    public class Menu
+    {
+        [Key]
+        [Required(ErrorMessage ="请输入菜单名")]
+        public int Id { get; set; }
+
+        [Display(Name = "菜单名")]
+        public string Name { get; set; }
+
+        [Display(Name = "url")]
+        public string Url { get; set; }
+
+        [Display(Name ="排序")]
+        public int Order { get; set; }
+
+        public Menu() {
+            Url = "";
+        }
+    }
+}
